2722c6432a21478755b75b070ec6a2d84fa02a23
emcmanus11 <elyse_mcmanus@brown.edu>
2013-11-18 20:08:10 -0500

    added dictionaries

--- walk_score.py
+++ walk_score.py
@@ -12,5 +12,11 @@ def main():
       dic[edge[1]]=(dic[edge[1]][0]+[edge[0],dic[edge[1]][1])
     else:
       dic[edge[1]]=([edge[1]],[])
+
+    intree = dict((key,(dic[key][0],dic[key][1])) for key in dic if not(len(dic[key][1])==len(dic[key][0]) and len(dic[key][0])==1) and len(dic[key][1])>0)
+    notintree = dict((key,(dic[key][0],dic[key][1])) for key in dic if len(dic[key][1])==len(dic[key][0]) and len(dic[key][0])==1)
+    newtree=intree
+
+main()
     
   
