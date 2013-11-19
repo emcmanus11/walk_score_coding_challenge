2edf21cb2f7651d82876d862a936bcff7fee7fa5
emcmanus11 <elyse_mcmanus@brown.edu>
2013-11-18 21:26:39 -0500

    debugging print statements

--- walk_score.py
+++ walk_score.py
@@ -1,10 +1,15 @@
 import fileinput
 from sets import Set
 
+
+
 def main():
   #dictionary of node --> (input nodes, output nodes)
   dic = dict()
   for line in fileinput.input():
+    if line.strip() == "DONE":
+      print "HI THERE"
+      break
     edge = line.split()
     if(edge[0] in dic):
       dic[edge[0]]=(dic[edge[0]][0],dic[edge[0]][1]+[edge[1]])
@@ -19,6 +24,10 @@ def main():
   newtree=intree
 
   while(notintree!={}):
+    print "dic: "+str(dic)
+    print "intree: "+str(intree)
+    print "notintree: "+str(notintree)
+    print("____________________")
     newtree={}
     for x in notintree:
       isinput = dict((a,intree[a]) for a in intree if x in intree[a][0])
@@ -29,18 +38,23 @@ def main():
           lis.remove(x)
           lis = Set(lis)
           lis.update(notintree[x][0])
-          intree[y]=(list(lis),intree[y][1])
+          intree[y]=(lis,intree[y][1])
       for y in isoutput:
         if y in intree:
           lis = intree[y][1]
           lis.remove(x)
           lis = Set(lis)
           lis.update(notintree[x][1])
-          intree[y]=(intree[y][0],list(lis))
+          intree[y]=(intree[y][0],lis)
 
     dic = intree
     intree = dict((key,(dic[key][0],dic[key][1])) for key in dic if not(len(dic[key][1])==len(dic[key][0]) and len(dic[key][0])==1) and len(dic[key][1])>0)
     notintree = dict((key,(dic[key][0],dic[key][1])) for key in dic if len(dic[key][1])==len(dic[key][0]) and len(dic[key][0])==1)
+    print "dic: "+str(dic)
+    print "intree: "+str(intree)
+    print "notintree: "+str(notintree)
+    print("_*_*_*_*_*_*_*_*_*_*_*_*_*")
+    
 
   for x in intree:
     for y in intree[x][1]:
